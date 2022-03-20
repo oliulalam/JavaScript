652dae259c3c4446789949d62b376f45990ae7b8
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-03-21 01:24:32 +0600

    JavaScript Basic

---
+++ Basic-js/14-11 Check whether a number is a Prime Number or not/checkPrime.js
@@ -0,0 +1,13 @@
+function isPrime(n)
+{
+    for(i=2; i<n; i++)
+    {
+        if(n%i==0)
+        {
+            return "Not prime number";
+        }
+    }
+    return "is prime number";
+}
+var result = isPrime(127);
+console.log(result);
