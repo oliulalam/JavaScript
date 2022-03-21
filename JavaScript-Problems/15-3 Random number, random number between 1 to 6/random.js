8d75909b47fe8b1175ee15bd560baadc87b85457
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-03-21 11:54:33 +0600

    JavaScript Basci

---
+++ JavaScript-Problems/15-3 Random number, random number between 1 to 6/random.js
@@ -0,0 +1,13 @@
+var randomNumber = Math.random() *6;
+var output = Math.round (randomNumber);
+console.log(output); 
+
+
+//using for loop 
+
+for (var i=0; i<=10; i++)
+{
+    var randomNumber = Math.random() *6;
+    var output = Math.round (randomNumber);
+    console.log(output);   
+}
---
+++ JavaScript-Problems/15-4 Find max of two values, find max of three values/max-value.js
@@ -0,0 +1,28 @@
+var busniess =450;
+var minister =550;
+var sochib =990;
+
+var max =Math.max(busniess,minister,sochib)
+console.log(max);
+
+/** 
+if(busniess>minister){
+    if(busniess>sochib)
+   {
+    console.log("Business Is bigger")
+   }
+
+else{
+    console.log("Sochin Is bigger")
+}
+}
+else{
+    if(minister>sochib)
+    {
+        console.log("Minister is bigger")
+    }
+    else{
+        console.log("Sochib is bigger")
+    }
+}
+*/
